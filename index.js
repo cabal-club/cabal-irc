62379fa9cba7c6a06cc62f5b50c48bdaac8642e5
Tony Ivanov <telamohn@gmail.com>
2018-11-17 00:12:31 +0100

    Upgrade cli argument handling & Channel listing
    * Upgraded cabal-core package to latest
    * Changed arguments handling to work similarly to `cabal`
    * Implemented channel-listing and join..
      AFAIK you can't selectively choose which channels to join/part once
      you're in the "cabal" muahaha. Thus, local in-memory channel handling
      is going to be removed and the client is forced to join all availale
      channels inside the cabal. We can implement a `/part` action later to
      mark some channels as 'ignored' by client. But good thing is that
      after the cabal-core upgrade we actually have some real cabal-data to
      work with!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Protocol = require('irc-protocol')
 const Cabal = require('cabal-core')
 const Swarm = require('cabal-core/swarm.js')
+const {promisify} = require('util')
 
 let log = function (...args) {
   let t = new Date()
@@ -8,6 +9,12 @@ let log = function (...args) {
     .map(i=> i.toString().padStart(2,'0'))
     .join(':')
   console.log.apply(null,[timestamp, ...args])
+}
+
+let debug = function (...args) {
+  if (true || process.env['DEBUG']) {
+    console.debug.apply(null, ['DEBUG:',...args])
+  }
 }
 
 // Expose all IRC numerics as lookup maps:
@@ -23,40 +30,93 @@ const Cmd = ((numerics) => {
 module.exports = class CabalIRC {
   constructor (storage, key, opts) {
     if (!opts) opts = {}
-
-    if (!storage && key) {
-      let homedir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
-      let rootdir = args.dir || (homedir + '/.cabal/archives/')
-      storage = rootdir + args.key
-    }
-
-    if (key) {
-      key = key.replace(/^(cabal|cbl|dat):\/\//).replace(/\//g, '')
-      this.cabal = Cabal(storage, key, opts)
-    } else {
-      this.cabal = Cabal(storage, null)
-    }
+    debug('Initializing core @', storage, 'with key:', key)
+    this.cabal = Cabal(storage, key, opts)
 
     this.hostname = opts.hostname || '127.0.0.1'
     this.users = {}
     this.channels = {}
+    debug('Waiting for cabal.db.ready')
     this.cabal.db.ready(() => {
+      debug('ready received')
       if (!key) this.cabal.getLocalKey((err, key) => {
         log(`New cabal instance created, public key:\ncabal://${key}`)
       })
-      this._onopen.bind(this)
+      this._onopen()
     })
   }
 
+  // Leaves the swarm
   quit () {
     if (this.swarm) this.swarm.destroy()
   }
 
+  // Joins the swarm and registers cabal event handlers
   _onopen () {
+    debug('Setting up swarm')
     this.swarm = Swarm(this.cabal)
-    log('joined swarm')
+    log('Joined swarm')
+    this.cabal.on('peer-added', peer => log('CAB:peer_added', peer))
+    this.cabal.on('peer-dropped', peer => log('CAB:peer_dropped', peer))
+
+    // Register handler for new channels.
+    this.cabal.channels.events.on('add', channel => this._channelAdd(channel))
+    // Register handler for new messages
+    this.cabal.messages.events.on('message', msg => this._messageAdd(msg))
   }
 
+  _channelAdd (channel) {
+    debug('CAB:channel_add', channel)
+    // This is a bit crappy, should probably have
+    // better way for sending server initiated comms
+    // than picking the first user in the users hash.
+    let user = Object.values(users)[0]
+    if (user) this._forceJoin(user)
+  }
+  _messageAdd (msg) {
+    debug('CAB:messag_glob', msg)
+    // Same problem here as in _channelAdd
+    let user = Object.values(users)[0]
+    if (user) this._echoMessage(msg)
+  }
+
+  // Forces connected client to join
+  // all available channels since from what I
+  // can see, cabal does not support join/part mechanics.
+  _forceJoin (user) {
+    return Promise.all([
+      promisify(this.cabal.users.getAll)(),
+      promisify(this.cabal.channels.get)()
+    ])
+      .then(([users, channels]) =>  {
+        channels.forEach(channel => {
+          user.socket.write(`:${user.nick} JOIN ${channel} \r\n`)
+          user.socket.write(`:${this.hostname} ${Cmd.RPL_TOPIC} ${user.nick} ${channel} :TODO TOPIC\r\n`)
+          let nicks = Object.values(users)
+            .map(u => u.name || u.key.substr(0,8))
+            .join(' ')
+          user.socket.write(`:${this.hostname} ${Cmd.RPL_NAMREPLY} ${user.nick} @ ${channel} :${nicks} \r\n`)
+          user.socket.write(`:${this.hostname} ${Cmd.RPL_ENDOFNAMES} ${user.nick} ${channel} :End of /NAMES list.\r\n`)
+        })
+      })
+      .catch(err => log(err))
+  }
+  _recapMessages (user) {
+    return promisify(this.cabal.channels.get)()
+      .then(channels => {
+        channels.forEach(channel => {
+          // TODO: grab the message parser from cabal project.
+          // no point reinventing the wheel.
+          let mio = this.cabal.messages.read(channel)
+          debugger
+          mio.on('data', message => {
+            debugger
+            this._echoMessage(user, message)
+          })
+        })
+      })
+      .catch(err => log(err))
+  }
 
   user (user, message) {
     delete this.users[user.nick]
@@ -70,6 +130,8 @@ module.exports = class CabalIRC {
     user.socket.write(`:${this.hostname} ${Cmd.RPL_MOTD} ${user.nick} :- Enjoy using your favourite IRC-software -\r\n`)
     user.socket.write(`:${this.hostname} ${Cmd.RPL_MOTD} ${user.nick} :- on the decentralized Cabal network      -\r\n`)
     user.socket.write(`:${this.hostname} ${Cmd.RPL_ENDOFMOTD} ${user.nick} :End of MOTD command\r\n`)
+    this._forceJoin(user)
+      .then(() => this._recapMessages(user))
   }
 
   nick (user, message) {
@@ -88,6 +150,13 @@ module.exports = class CabalIRC {
   }
 
   privmsg (user, message) {
+    /*this.cabal.publish({
+      type: 'chat/text',
+        content: {
+          text: 'hello world',
+            channel: 'cabal-dev'
+        }
+    })*/
     log(message)
   }
 
@@ -107,10 +176,15 @@ module.exports = class CabalIRC {
   }
 
   mode (user, message) {
+    // Pacifier/Dummy always returns +ns modes to the client.
     let channel = message.parameters[0]
-    user.socket.write(`:${this.hostname} ` + channel + '+ns\r\n')
+    user.socket.write(`:${this.hostname} ${Cmd.RPL_UMODEIS} ${channel} +ns\r\n`)
   }
 
+  // Dropped internal channel-objects, using
+  // cabal-views directly, see _forceJoin()
+  // for new operation.
+  /*
   join (user, message) {
     let channel = message.parameters[0]
     let {channels, hostname} = this
@@ -129,6 +203,7 @@ module.exports = class CabalIRC {
     user.socket.write(`:${hostname} ${Cmd.RPL_NAMREPLY} ${user.nick} @ ${channel} :${nicks} \r\n`)
     user.socket.write(`:${hostname} ${Cmd.RPL_ENDOFNAMES} ${user.nick} ${channel} :End of /NAMES list.\r\n`)
   }
+*/
 
   list (user, message) {
     this.cabal.channels.get((err, channels) => {
@@ -175,5 +250,7 @@ module.exports = class CabalIRC {
 
   notImplemented (user, cmd) {
     console.error('not implemented', cmd)
+    // Produces garbage
+    // user.socket.write(`:${this.hostname} ${Cmd.ERR_UNKNOWNCOMMAND} ${cmd} :Command not implemented"\r\n`)
   }
 }
